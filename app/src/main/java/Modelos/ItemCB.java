3f732f93f54e39397e86bc078d10e25990d40fc9
agregarFoto <ncedenob@uteq.edu.ec>
2024-06-28 22:31:29 -0500

    Initial commit

---
+++ app/src/main/java/Modelos/ItemCB.java
@@ -0,0 +1,38 @@
+package Modelos;
+
+import androidx.annotation.NonNull;
+
+public class ItemCB {
+
+    int ID;
+    String Descripcion;
+
+    @NonNull
+    @Override
+    public String toString() {       return Descripcion;    }
+
+    public ItemCB(int ID, String descripcion) {
+        this.ID = ID;
+        Descripcion = descripcion;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getDescripcion() {
+        return Descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        Descripcion = descripcion;
+    }
+
+
+
+}
+
